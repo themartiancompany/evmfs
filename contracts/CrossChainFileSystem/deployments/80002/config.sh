c45eb136edd3fa0821bf9059e9bb470516d83009
Truocolo (kf) <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-06-20 00:37:33 +0200

    mm

--- contracts/CrossChainFileSystem/deployments/80002/config.sh
+++ contracts/CrossChainFileSystem/deployments/80002/config.sh
@@ -7,10 +7,10 @@ declare \
   contract_address
 
 solc_version=(
-  ["1.0"]="0.8.24"
+  ["1.0"]="0.8.28"
 )
 evm_version=(
-  ["1.0"]="paris"
+  ["1.0"]="cancun"
 )
 contract_address=(
   ["1.0"]="0x629d369Edd5f64391f0bA6187F2794D7b54F7e57"
