0ddc4376451be4d382d1dc8d1e8e81886aec3fe3
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2025-06-22 12:44:52 +0000

    wut

--- contracts/LengthLock/deployments/61/config.sh
+++ contracts/LengthLock/deployments/61/config.sh
@@ -7,10 +7,10 @@ declare \
   contract_address
 
 solc_version=(
-  ["1.0"]="0.8.28"
+  ["1.0"]="0.8.24"
 )
 evm_version=(
-  ["1.0"]="cancun"
+  ["1.0"]="paris"
 )
 contract_address=(
   ["1.0"]="0xb0e225407f01658b3f42536D1615B3679708e1d8"
