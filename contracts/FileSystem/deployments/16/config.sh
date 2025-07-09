bd07e0e59ebe7f436689b0efe4ae6e87e1f960b2
Truocolo (kf) <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-07-09 20:31:14 +0200

    seems this testnet does not support cancun

--- contracts/FileSystem/deployments/16/config.sh
+++ contracts/FileSystem/deployments/16/config.sh
@@ -7,11 +7,11 @@ declare \
   contract_address
 
 solc_version=(
-  ["1.0"]="0.8.28"
+  ["1.0"]="0.8.24"
 )
 evm_version=(
-  ["1.0"]="cancun"
+  ["1.0"]="paris"
 )
 contract_address=(
-  ["1.0"]="0x7D55E8B250DC2393255d62db57C4C8bF7BCf23ec"
+  ["1.0"]="0xb5bd86780291bE07b1A83F982BbD2A8dcd4e59aA"
 )
